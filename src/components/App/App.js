c557157b6664fe94edb552e3ea68599f41d48957
ArshiaKSabet <arshia.kolachaei@hotmail.com>
2022-05-16 11:52:23 +0200

    First draft

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import { Typography, Button } from "@mui/material";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Typography sx={{ color: "primary.main" }}>Spotify Clone</Typography>
+      <Button variant="contained">Click me</Button>
+    </div>
+  );
+}
+
+export default App;
